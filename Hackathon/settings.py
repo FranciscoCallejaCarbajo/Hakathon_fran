933d161b68d2d20208e23106b81e3edb66649849
FranciscoCallejaCarbajo <160488986+FranciscoCallejaCarbajo@users.noreply.github.com>
2024-09-09 09:46:06 +0200

    update settings

--- Hackathon/settings.py
+++ Hackathon/settings.py
@@ -12,7 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+# DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']  # Puedes cambiar esto si deseas restringir los hosts permitidos
 
