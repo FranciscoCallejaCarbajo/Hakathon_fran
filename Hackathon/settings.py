1c77db2f1f0a5439837fbf22481fb727bb556e80
FranciscoCallejaCarbajo <160488986+FranciscoCallejaCarbajo@users.noreply.github.com>
2024-09-09 10:28:11 +0200

    update debug

--- Hackathon/settings.py
+++ Hackathon/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
-# DEBUG = True
+# DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
